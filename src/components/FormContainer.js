d3b37ba6421ac9b8b248d7a72467d5fb0523cf77
watarun <watarun54@gmail.com>
2018-10-18 14:51:43 +0900

    Adjust the overall design and adopt esponsive web design.

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DropdownBtn from './card-components/DropdownBtn';
-import { InputGroup, InputGroupAddon, InputGroupButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, Input} from 'reactstrap'; 
+import { InputGroup, InputGroupAddon, Button, Input} from 'reactstrap'; 
 
 
 class FormContainer extends React.Component {
@@ -27,19 +27,6 @@ class FormContainer extends React.Component {
                 <InputGroup>
                     <Input value={this.state.item} placeholder="New task" onChange={(e) => this.setState({ item: e.target.value })}/>
                     <DropdownBtn selectedPriority={this.state.selectedPriority} handleSetPriority={this.handleSetPriority}/>
-                    {/*
-                    <InputGroupButtonDropdown addonType="append" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
-                        <DropdownToggle caret>
-                            
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem className="eme-imp-button" onClick={() => this.onRadioBtnClick(0)}>緊急＆重要</DropdownItem>
-                            <DropdownItem className="eme-button" onClick={() => this.onRadioBtnClick(1)}>緊急</DropdownItem>
-                            <DropdownItem className="imp-button" onClick={() => this.onRadioBtnClick(2)}>重要</DropdownItem>
-                        </DropdownMenu>
-                    </InputGroupButtonDropdown>
-                
-                    */}
                     <InputGroupAddon addonType="prepend"><Button onClick={this.handleSubmit}>Add</Button></InputGroupAddon>
                 </InputGroup>
             </div>
